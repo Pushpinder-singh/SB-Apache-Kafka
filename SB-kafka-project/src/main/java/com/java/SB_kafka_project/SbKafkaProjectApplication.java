df5ae73270a40100136a07d977c72a83af7d68c8
Pushpinder-singh <pushpinder.1990singh@gmail.com>
2025-05-14 12:49:37 -0400

    Update SbKafkaProjectApplication.java
    Boot the application

--- SB-kafka-project/src/main/java/com/java/SB_kafka_project/SbKafkaProjectApplication.java
+++ SB-kafka-project/src/main/java/com/java/SB_kafka_project/SbKafkaProjectApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SbKafkaProjectApplication {
 
+	// Spring boot run application
 	public static void main(String[] args) {
 		SpringApplication.run(SbKafkaProjectApplication.class, args);
 	}
