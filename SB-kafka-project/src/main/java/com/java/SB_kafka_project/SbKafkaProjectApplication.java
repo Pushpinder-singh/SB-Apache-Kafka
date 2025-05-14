6686aac105b06acbbb787945923db107f5764bb1
Pushpinder-singh <pushpinder.1990singh@gmail.com>
2025-05-14 12:46:42 -0400

    Add files via upload

---
+++ SB-kafka-project/src/main/java/com/java/SB_kafka_project/SbKafkaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.java.SB_kafka_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbKafkaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbKafkaProjectApplication.class, args);
+	}
+
+}
