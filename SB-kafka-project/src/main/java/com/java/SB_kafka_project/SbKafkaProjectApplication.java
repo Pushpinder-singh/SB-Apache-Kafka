cd36adeaed5b736d10c21ee27cb55e7124648f2e
pushpinder singh <pushpinder.1990singh@gmail.com>
2025-05-20 09:05:58 -0400

    Updated Main file

--- SB-kafka-project/src/main/java/com/java/SB_kafka_project/SbKafkaProjectApplication.java
+++ SB-kafka-project/src/main/java/com/java/SB_kafka_project/SbKafkaProjectApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SbKafkaProjectApplication {
 
 	// Spring boot run application
+	// Streams API is hihhly used in this project
 	public static void main(String[] args) {
 		SpringApplication.run(SbKafkaProjectApplication.class, args);
 	}
